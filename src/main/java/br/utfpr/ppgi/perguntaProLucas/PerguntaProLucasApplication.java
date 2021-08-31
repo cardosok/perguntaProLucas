7ab21b47d8633eb685fb1697038e3eeb4d62a72b
lucasyoshioka1 <lucasyoshioka1@xbrain.com.br>
2021-08-30 21:02:03 -0300

    First commit

---
+++ src/main/java/br/utfpr/ppgi/perguntaProLucas/PerguntaProLucasApplication.java
@@ -0,0 +1,13 @@
+package br.utfpr.ppgi.perguntaProLucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerguntaProLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerguntaProLucasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/utfpr/ppgi/perguntaProLucas/PerguntaProLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.ppgi.perguntaProLucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerguntaProLucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
