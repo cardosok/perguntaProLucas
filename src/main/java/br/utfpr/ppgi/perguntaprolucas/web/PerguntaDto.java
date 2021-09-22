b24017f6c3f04843e086610cbc02bc105bd59e7e
Jaumzera <joaomassan@gmail.com>
2021-09-21 21:24:29 -0300

    SM-18 Serviço pegando perguntas do banco

--- src/main/java/br/utfpr/ppgi/perguntaprolucas/web/PerguntaDto.java
+++ src/main/java/br/utfpr/ppgi/perguntaprolucas/web/PerguntaDto.java
@@ -2,30 +2,30 @@ package br.utfpr.ppgi.perguntaprolucas.web;
 
 import br.utfpr.ppgi.perguntaprolucas.domain.Opcao;
 import br.utfpr.ppgi.perguntaprolucas.domain.Pergunta;
+import br.utfpr.ppgi.perguntaprolucas.domain.TipoPergunta;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Value
 @Builder
 @AllArgsConstructor
 public class PerguntaDto {
 
-    String texto;
+  String texto;
 
-    List<String> opcoes;
+  List<String> opcoes;
 
-    public static PerguntaDto from(Pergunta perguntaAtual) {
-        return PerguntaDto.builder()
-                .texto(perguntaAtual.getTexto())
-                .opcoes(perguntaAtual.getOpcoes()
-                        .stream()
-                        .map(Opcao::getTexto)
-                        .collect(Collectors.toList()))
-                .build();
-    }
+  TipoPergunta tipoPergunta;
 
+  public static PerguntaDto from(Pergunta perguntaAtual) {
+    return PerguntaDto.builder()
+        .texto(perguntaAtual.getTexto())
+        .tipoPergunta(perguntaAtual.getTipoPergunta())
+        .opcoes(
+            perguntaAtual.getOpcoes().stream().map(Opcao::getTexto).collect(Collectors.toList()))
+        .build();
+  }
 }
